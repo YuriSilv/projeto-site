7c5925211051b5983145773235376732bbc72572
Yuri Silvino dos Santos <63326649+YuriSilv@users.noreply.github.com>
2021-03-19 18:29:03 -0300

    fix Grid

--- Codigos/Timer.js
+++ Codigos/Timer.js
@@ -7,7 +7,7 @@ function colorTheme()
     var date = new Date();
     var hour = date.getHours();
     var min = date.getMinutes();
-
+    
     if(hour > 6 && hour <= 12) {
         header.style.backgroundColor = "#558564"
         document.body.style.backgroundColor = "#D4EAC8"
@@ -16,7 +16,7 @@ function colorTheme()
             document.getElementsByTagName("a")[index].style.color = "#F3A712"   
         }
 
-    }else if (hour >= 18) {
+    }else if (hour >= 18 || hour >= 0 && hour <= 6) {
         header.style.backgroundColor = "#0E1116"
         document.body.style.backgroundColor = "#374A67"
         
